f25d98846a8f3ad4de8027767f09f09a3e558c97
Abrar Zahin <abrar.zahin2023@gmail.com>
2025-09-20 21:54:47 +0600

    Add files via upload

---
+++ Assignment Operators/Assignment Operators.c
@@ -0,0 +1,20 @@
+Operator     Example       Same As
+=      x = 5      x = 5
++=     x += 3     x = x + 3
+-=     x -= 3     x = x - 3
+*=     x *= 3     x = x * 3
+/=     x /= 3     x = x / 3
+%=     x %= 3     x = x % 3
+&=     x &= 3     x = x & 3
+|=     x |= 3     x = x | 3
+^=     x ^= 3     x = x ^ 3
+>>=    x >>= 3    x = x >> 3
+<<=    x <<= 3    x = x << 3
+
+
+// formate
+
+int savings = 100;
+ savings += 50; // add 50 to savings
+
+ printf("Total savings: %d\n", savings);
