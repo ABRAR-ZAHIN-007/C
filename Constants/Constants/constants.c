3404fc5afe68b532fcb526e0b3d9a667653ba5ac
Abrar Zahin <abrar.zahin2023@gmail.com>
2025-09-20 21:52:53 +0600

    Add files via upload

---
+++ Constants/Constants/constants.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Constants
+//
+//  Created by Abrar Zahin on 9/20/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    //This will declare the variable as "constant", which means unchangeable and read-only
+    
+    const int BIRTHYEAR = 1980;
+      
+      printf("%d", BIRTHYEAR);
+    return EXIT_SUCCESS;
+}
